5c27e7cc927892b7f4262973e34c6d92bbcf83ea
pawanjot913 <pawanjot913@gmail.com>
2026-02-14 03:42:51 +0530

    deployed

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -2,13 +2,14 @@ import axios from "axios";
 import httpStatus from "http-status";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import server from "../environment";
+import server from "../utils/environment";
+import server from "../environment";
 
 
 export const AuthContext = createContext({});
 
 const client = axios.create({
-    baseURL: `http://localhost:3000/users`
+    baseURL: `${server}/users`
 })
 
 
--- frontend/src/utils/environment.js
+++ frontend/src/utils/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://apna-video-call-1-4ydh.onrender.com" :
 
     "http://localhost:5000";
 
